a750d5ba587a0a818c4d152156ddfd55bfd136f6
Emoji <55009343+Emojigit@users.noreply.github.com>
2022-05-29 19:06:59 +0800

    rm dup and allow param in username

--- __init__.py
+++ __init__.py
@@ -17,21 +17,23 @@ except AttributeError:
 TWClient = tw.AsyncClient(bearer_token=tw_api_bearer,return_type=dict)
 tweetid_regex = re.compile(r"(http(s?)://twitter.com/([a-zA-Z0-9_-]+)/status/([0-9]+))(\?[a-zA-Z0-9%&=]+)?") # .match
 tweetid_reply = "[Tweet Data]({url})\n__Tweeted by__: [{dname}](https://twitter.com/{by})\n__Tweeted at__: {time}\n{text}"
-user_regex = re.compile(r"http(s?)://twitter.com/([a-zA-Z0-9_-]+)/? ") # UNAME @ GP2
+user_regex = re.compile(r"http(s?)://twitter.com/([a-zA-Z0-9_-]+)/?(\?[a-zA-Z0-9%&=]+)? ") # UNAME @ GP2
 user_reply = "[{dname}](https://twitter.com/{uname}){verified}\n__Location__: {location}\n__URL__: {url}\n__Description__: {description}"
 nf = "404 Not Found"
+def rmduplicate(l):
+    return list(dict.fromkeys(l))
 def setup(bot,storage):
     @bot.on(events.NewMessage())
     async def tw_trymatch(event):
         text = event.text
-        for x in tweetid_regex.findall(text):
+        for x in rmduplicate(tweetid_regex.findall(text)):
             #await event.respond(str(x))
             id = int(x[3])
             tweet = await TWClient.get_tweet(id=id,expansions=["author_id"],tweet_fields=["created_at","author_id"],user_fields=["name"])
             #await event.respond(str(tweet))
             text = tweet["data"]["text"]
             await event.reply(tweetid_reply.format(time=tweet["data"]["created_at"],by=x[2],text=text,url=x[0],dname=tweet["includes"]["users"][0]["name"]),link_preview=False)
-        for x in user_regex.findall(text + " "):
+        for x in rmduplicate(user_regex.findall(text + " ")):
             uname = x[1]
             user = (await TWClient.get_user(username=uname,user_fields=["description","location","verified","url"]))["data"]
             #await event.respond(str(user))
